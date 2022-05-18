b7045f0d491fcb91efe29119a60f985eeb9d65c4
Minino99 <loizzo.pasqualino@gmail.com>
2022-05-18 17:15:23 +0200

    bonus e cleaning manca blocco script x errore

--- js/script.js
+++ js/script.js
@@ -19,20 +19,37 @@ posso inserire un minimo / massimo di km possibili?
 */
 
 let km = prompt("Inserisci il numero di chilometri che vuoi percorrere");
+
+if (km > 1200 || km < 10) {
+ alert("Attenzione! Il numero di chilometri deve essere compreso tra 10 e 1200");
+}
+
 let age = prompt("Inserisci la tua età");
+
+if (age < 0 || age > 120 || isNaN(age)) {
+ alert("Inserisci un'età valida");
+}
+
 let price = km * 0.21;
 const youngdiscount = 0.2;
 const elderdiscount = 0.4;
-const totalprice = document.getElementById("totalprice");
+let totalprice = 0;
 
 if (age < 18) {
-  totalprice.innerHTML = (price - price * youngdiscount).toFixed(2);
-} else totalprice.innerHTML = price;
+  totalprice = (price - price * youngdiscount).toFixed(2);
+}
+
 if (age > 65) {
-  totalprice.innerHTML = (price - price * elderdiscount).toFixed(2);
+  totalprice = (price - price * elderdiscount).toFixed(2);
+}
+
+if (age >= 18 && age <= 65) {
+  totalprice = price.toFixed(2);
 }
 
 const displaykm = document.getElementById("kminput");
 const displayage = document.getElementById("ageinput");
+const displaytotal = document.getElementById("totalprice");
 displaykm.innerHTML = km;
 displayage.innerHTML = age;
+displaytotal.innerHTML = totalprice;
